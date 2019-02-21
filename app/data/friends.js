c9c57841f5ea3c4850f78ba05d383237e1fa119c
Preston Carey <pcarey1213@gmail.com>
2019-02-20 22:24:39 -0500

    heroku commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,65 +1,44 @@
 var friendsData=[
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },
-  {
-    "name":"Steve",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Steven",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/2/22/Steven_Universe.png",
     "scores":[ 3, 4, 2, 1, 2, 1, 1, 1, 4, 5 ]
 
   },
   {
     "name":"Jerry",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://pixel.nymag.com/imgs/daily/vulture/2018/06/07/14-jerry-springer.w330.h330.jpg",
     "scores":[ 2, 1, 2, 4, 1, 4, 1, 1, 3, 2 ]
   },
   {
   "name":"Terry",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDYwNjkzMjY3OTgz/terry-crews-headshot-600x600jpg.jpg",
   "scores":[ 2, 1, 1, 4, 4, 4, 2, 4, 2, 1 ]
   },
   {
     "name":"Rita",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://m.media-amazon.com/images/M/MV5BMTQxOTk1NTQ1M15BMl5BanBnXkFtZTcwMjI1MDAwNA@@._V1_.jpg",
     "scores":[ 1, 5, 3, 1, 4, 5, 1, 1, 4, 4 ]
   },
     {
     "name":"Susan",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://www.irishcentral.com/uploads/article/128680/cropped_MI_Susan_Boyle.jpg?t=1538567508",
     "scores":[ 2, 4, 4, 5, 3, 2, 1, 4, 5, 3 ]
     },
     {
     "name":"Sandra",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://cdn-images-1.medium.com/max/1200/1*aVVn9i1q1kh_1dvfEC0rQA@2x.jpeg",
     "scores":[ 5, 1, 3, 1, 3, 4, 2, 2, 1, 4 ]
     },
     {
     "name":"Bob",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://a57.foxnews.com/media2.foxnews.com/BrightCove/694940094001/2019/01/16/931/524/694940094001_5990121284001_5990122154001-vs.jpg?ve=1&tl=1",
     "scores":[ 4, 4, 1, 1, 1, 4, 5, 1, 2, 5 ]
     },
     {
-    "name":"Jeffrey",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Geoffrey",
+    "photo":"https://www.wsws.org/asset/66d45bbb-b03e-4a05-9cb8-2a20ed94ae3L/image.jpg?rendition=image480https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[ 5, 2, 1, 5, 5, 3, 1, 1, 5, 4 ]
-    },
-    {
-    "name":"Marcus",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[ 4, 3, 1, 4, 1, 5, 4, 1, 2, 2 ]
     }
 ];
 
